10deb8a57221bdc9b74d5ee08fb0e7c0c95a3a5a
David Khazzam <david.a.khazzam@gmail.com>
2024-07-29 12:35:01 -0400

    Changed what a hot day is

--- include/global_variables/user_input_variables.py
+++ include/global_variables/user_input_variables.py
@@ -4,5 +4,5 @@
 
 MY_NAME = "David"
 MY_CITY = "New York City"
-HOT_DAY = 30 # modify what you consider a hot day in degree Celsius
+HOT_DAY = 28 # modify what you consider a hot day in degree Celsius
 BIRTH_YEAR = 2000
